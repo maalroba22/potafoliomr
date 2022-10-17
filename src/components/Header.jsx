d2f4af3b1890c9704cb8ce928354c9bcdd144e61
maalroba22 <maalroba22@gmail.com>
2022-10-16 21:10:16 -0500

    boton cerrar e desplegar en nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Nav from './Nav';
-import { RiAlignJustify } from 'react-icons/ri'
+import { RiAlignJustify,RiCloseLine } from 'react-icons/ri'
+import logo from '../assets/img/logomr1.png'
 
 const Header = () => {
+    const [showMenu, setshowMenu] = useState(false);
+
     return (
-        <div className=' flex items-center w-full p-4  h-[10vh]' >
-            <div className=' w-1/6 text-center'>Logo</div>
+        <div className=' flex items-center justify-between xl:justify-start w-full h-[10vh] p-4 z-50' >
+            <div className='xl:w-1/6 text-center'>
+                <img className=" h-12 w-full" src={logo} alt="Logo"  />
+            </div>
             {/* menu Navehgcion */}
-            <div ><Nav /></div>
-            <button>
-                <RiAlignJustify />
+            <div ><Nav  mostar ={showMenu}/></div>
+            <button onClick={()=>setshowMenu(!showMenu)} className="text-2xl p-2 xl:hidden">
+             
+            {!showMenu ? <RiAlignJustify/> : <RiCloseLine/> }
             </button>
         </div>
     );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {RiCloseLine} from 'react-icons/ri'
 
-const Nav = () => {
+
+const Nav = ({mostar}) => {
+    
     return (
-        <div className='fixed bg-gray-300  w-full h-full right-0 top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10'>
-            <button><RiCloseLine/></button>
+        <div className={`fixed bg-white  w-[80%] md:w-[40%] xl:w-full h-full ${mostar ? "left-0" :"-left-full" }   top-0 xl:static
+         flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500`}>
+           
             <a href="#cualquier ruta">Home1</a>
             <a href="#cualquier ruta">Home2</a>
             <a href="#cualquier ruta">Home3</a>
